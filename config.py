43a2c57239b21038dbbdf245a0cce170169fa43c
Botmaster55 <wajsarif461@gmail.com>
2026-03-27 17:30:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,10 +29,10 @@ START_PIC = os.environ.get("START_PIC", "https://image.zaw-myo.workers.dev/image
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://image.zaw-myo.workers.dev/image/8d7703e1-5f8a-4cfb-88b9-bf3d8ebb8caa")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkshortify.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "74e17137f92e31cc0406fab6fcf3131bc61f8ecc")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 42100)) # Add time in seconds
-TUT_VID = os.environ.get("TUT_VID","")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/Premium_Jaction/456")
 
 #--------------------------------------------
 
